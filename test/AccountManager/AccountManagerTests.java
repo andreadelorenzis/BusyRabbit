b08142bab514b22f32194c2ff637a4b3116596d3
Andrea De Lorenzis <andreadelorenzis99@gmail.com>
2022-01-26 14:17:18 +0100

    Aggiunge test per i package TimeTracker e AccountManager

---
+++ test/AccountManager/AccountManagerTests.java
@@ -0,0 +1,223 @@
+package AccountManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import org.junit.jupiter.api.Test;
+
+import main.Colori;
+import main.Models.AccountManager.classes.App;
+import main.Models.AccountManager.classes.ExistingAccountException;
+import main.Models.AccountManager.classes.WrongCredentialsException;
+import main.Models.AccountManager.interfaces.IApp;
+import main.Models.goalmanager.classes.ObiettivoScomponibile;
+import main.Models.goalmanager.interfaces.IAzioneScomponibile;
+import main.Models.goalmanager.interfaces.IGoalManager;
+import main.Models.goalmanager.interfaces.IObiettivoAzione;
+import main.Models.goalmanager.interfaces.IObiettivoScomponibile;
+import main.Models.habittracker.interfaces.IHabitTracker;
+import main.Models.habittracker.interfaces.ISimpleHabit;
+import main.Models.timetracker.interfaces.IAttività;
+import main.Models.timetracker.interfaces.IProgetto;
+import main.Models.timetracker.interfaces.ITimeTracker;
+
+/**
+ * TODO:
+ * - testare lettura ora inizio attività
+ * 
+ */
+class AccountManagerTests {	
+	
+	private void accedi(IApp app) {
+		// accede all'account presente nel database
+		try {
+			app.accedi("andredelo@gmail.com", "pass123");
+		} catch (WrongCredentialsException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testAccesso() {
+		IApp app = new App();
+		
+		// prova ad accedere ad un account sbagliando email
+		assertThrows(WrongCredentialsException.class, () -> app.accedi("andredelo@gmail.co", "pass123"));
+		assertFalse(app.getAccessoEffettuato());
+		
+		// prova ad accedere ad un account sbagliando password
+		assertThrows(WrongCredentialsException.class, () -> app.accedi("andredelo@gmail.com", "pass13"));
+		assertFalse(app.getAccessoEffettuato());
+		
+		// accede ad un account esistente
+		accedi(app);
+		
+		// accesso effettuato
+		assertTrue(app.getAccessoEffettuato());
+
+	}
+	
+	@Test
+	public void testRegistrazione() {
+		IApp app = new App();
+		
+		// prova a creare un nuovo account, ma le due password non corrispondono
+		assertThrows(WrongCredentialsException.class, () -> app.registraAccount("Andre", "andre@gmail.com", "pass123", "pass124"));
+		assertFalse(app.getAccessoEffettuato());
+		assertFalse(new File("database/andre@gmail.com.txt").exists());
+		
+		// prova a creare un nuovo account, ma l'email "andredelo@gmail.com" è già stata usata
+		assertThrows(ExistingAccountException.class, () -> app.registraAccount("Andre", "andredelo@gmail.com", "pass123", "pass123"));
+		
+		// registra un nuovo account con successo
+		try {
+			app.registraAccount("Andre", "andre@gmail.com", "pass123", "pass123");
+		} catch (WrongCredentialsException | ExistingAccountException e) {
+			e.printStackTrace();
+		}
+		assertTrue(app.getAccessoEffettuato());
+		assertTrue(new File("database/andre@gmail.com.txt").exists());
+		
+		// elimina l'account
+		try {
+			app.eliminaAccount("andre@gmail.com", "pass123");
+		} catch (WrongCredentialsException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test 
+	public void testEliminazione() {
+		IApp app = new App();
+		
+		// crea un nuovo account
+		try {
+			app.registraAccount("Andre", "andre@gmail.com", "pass123", "pass123");
+		} catch (WrongCredentialsException | ExistingAccountException e) {
+			e.printStackTrace();
+		}
+		
+		// prova a eliminare un account, sbagliando la password
+		assertThrows(WrongCredentialsException.class, () -> app.eliminaAccount("andre@gmail.com", "pass12"));
+		assertTrue(new File("database/andre@gmail.com.txt").exists());
+		
+		// elimina l'account
+		try {
+			app.eliminaAccount("andre@gmail.com", "pass123");
+		} catch (WrongCredentialsException e) {
+			e.printStackTrace();
+		}
+		assertFalse(new File("database/andre@gmail.com.txt").exists());
+	}
+	
+	/**
+	 * Verifica la corretta lettura da file dei dati del TimeTracker
+	 */
+	@Test
+	public void testLetturaTimeTracker() {
+		IApp app = new App();
+		ITimeTracker tt = app.getTT();
+		
+		// accede all'account presente nel database
+		accedi(app);
+		
+		// sono presenti 3 progetti, compreso quello di default ("Altro")
+		assertEquals(3, tt.getProgetti().size());
+		// controlla primo progetto letto
+		IProgetto p = tt.getProgetti().get(1);
+		assertEquals("PMO", p.getNome());
+		assertEquals(Colori.Blu, p.getColore());
+		assertEquals("idP1", p.getId());
+		
+		// sono presenti 2 attività
+		assertEquals(2, tt.getAttività().size());
+		// controlla prima attività
+		IAttività a = tt.getAttività().get(0);
+		assertEquals("Studiare analisi", a.getNome());
+		assertEquals(25, a.getData().getDayOfMonth());
+		assertEquals(5, a.getData().getMonthValue());
+		assertEquals(2022, a.getData().getYear());
+		assertEquals(7200, a.getDurata());
+		assertEquals("idP1", a.getProgetto().getId());
+		assertEquals("idA1", a.getId());
+		
+	}
+	
+	/**
+	 * Verifica la corretta lettura da file dei dati del GoalManager
+	 */
+	@Test
+	public void testLetturaObiettivi() {
+		IApp app = new App();
+		IGoalManager gm = app.getGM();
+		
+		// accede all'account presente nel database
+		accedi(app);
+		
+		// ci sono 3 obiettivi
+		assertEquals(3, gm.getObiettivi().size());
+		// primo obiettivo, "Dare gli esami"
+		assertTrue(gm.getObiettivi().get(0) instanceof ObiettivoScomponibile);
+		IObiettivoScomponibile o1 = (IObiettivoScomponibile) gm.getObiettivi().get(0);
+		assertEquals("Dare gli esami", o1.getNome());
+		assertEquals("nessuna descrizione", o1.getDescrizione());
+		// "Dare gli esami" ha due sotto-obiettivi: "dare analisi" e "dare pmo"
+		assertEquals(2, o1.getSottoObiettivi().size());
+		IObiettivoScomponibile o3 = (IObiettivoScomponibile) o1.getSottoObiettivi().get(0);
+		IObiettivoScomponibile o4 = (IObiettivoScomponibile) o1.getSottoObiettivi().get(1);
+		assertEquals("dare analisi", o3.getNome());
+		assertEquals("dare pmo", o4.getNome());
+		// secondo obiettivo, "creare una startup"
+		IObiettivoAzione o2 = (IObiettivoAzione) gm.getObiettivi().get(1);
+		// l'obiettivo ha 2 azioni collegate, "lavorare al prodotto" e "fare pubblicità"
+		assertEquals(2, o2.getAzioni().size());
+		IAzioneScomponibile a1 = (IAzioneScomponibile) o2.getAzioni().get(0);
+		IAzioneScomponibile a2 = (IAzioneScomponibile) o2.getAzioni().get(1);
+		assertEquals("lavorare al prodotto", a1.getNome());
+		assertEquals("fare pubblicita", a2.getNome());
+		// la prima azione si compone di 2 item, "programmare" e "fare design"
+		assertEquals(2, a1.getItems().size());
+		assertEquals("programmare", a1.getItems().get(0).getNome());
+		assertEquals("fare design", a1.getItems().get(1).getNome());
+	}
+	
+	@Test 
+	public void testLetturaAbitudini() {
+		IApp app = new App();
+		IHabitTracker ht = app.getHT();
+		
+		// accede all'account presente nel database
+		accedi(app);
+		
+		// ci sono 2 abitudini
+		assertEquals(2, ht.getHabits().size());
+		// controllo la prima abitudine semplice
+		ISimpleHabit h = (ISimpleHabit) ht.getHabits().get(0);
+		assertEquals("routine mattutina", h.getName());
+		assertEquals("descrizione", h.getDescription());
+		assertEquals(20, h.getStartDate().getDayOfMonth());
+		assertEquals(2, h.getStartDate().getMonthValue());
+		assertEquals(2022, h.getStartDate().getYear());
+		assertEquals(2, h.getDays().size());
+		assertEquals("idAb1", h.getId());
+		// l'abitudine si compone di due item abitudine
+		assertEquals(2, h.getItems().size());
+		// primo item
+		assertEquals("doccia fredda", h.getItems().get(0).getNome());
+		assertEquals("idI1", h.getItems().get(0).getId());
+	}
+	
+	@Test
+	public void testLetturaImpostazioni() {
+		IApp app = new App();
+		
+		// accede all'account presente nel database
+		accedi(app);
+		
+		// sfondo-scuro: true
+		assertTrue(app.getSfondoScuro());
+		// username: andre
+		assertEquals("andre", app.getUsername());
+	}
+
+}
---
+++ test/TimeTracker/TimeTrackerTests.java
@@ -0,0 +1,415 @@
+package TimeTracker;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import main.Colori;
+import main.Models.timetracker.classes.Attività;
+import main.Models.timetracker.classes.Progetto;
+import main.Models.timetracker.classes.TimeTracker;
+import main.Models.timetracker.classes.TrackerEnum;
+import main.Models.timetracker.interfaces.IAttività;
+import main.Models.timetracker.interfaces.ICronometro;
+import main.Models.timetracker.interfaces.IPomodoroTimer;
+import main.Models.timetracker.interfaces.IProgetto;
+import main.Models.timetracker.interfaces.ITimeTracker;
+import java.util.List;
+import java.util.Map;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TimeTrackerTests {
+
+	ITimeTracker t = null;
+	
+	private void inizializza(ITimeTracker t) {
+		// creo 2 progetti
+		t.aggiungiProgetto(new Progetto("Analisi", Colori.Rosso));
+		t.aggiungiProgetto(new Progetto("Allenamento", Colori.Verde));
+
+		// aggiungo 3 attività al primo progetto
+		t.aggiungiAttività(new Attività("Studiare",
+										LocalDate.of(2021, Month.JANUARY, 2),
+										LocalTime.of(19, 0),
+										7200L,
+										t.getProgetti().get(1)));
+		t.aggiungiAttività(new Attività("Fare esercizi",
+										LocalDate.of(2021, Month.JANUARY, 10),
+										LocalTime.of(12, 15),
+										7200L,
+										t.getProgetti().get(1)));
+		t.aggiungiAttività(new Attività("Studiare ancora",
+										LocalDate.of(2021, Month.FEBRUARY, 5),
+										LocalTime.of(7, 2),
+										7200L,
+										t.getProgetti().get(1)));
+		t.aggiungiAttività(new Attività("Gambe",
+										LocalDate.of(2022, Month.MARCH, 7),
+										LocalTime.of(15, 55),
+										3600L));
+	}
+	
+	private void aggiungiAttività(ITimeTracker t) {
+		
+		// creo un nuovo progetto
+		t.aggiungiProgetto(new Progetto("Progetto", Colori.Rosso));
+		
+		// aggiungo 30 attività, in 30 giorni diversi, associate al progetto
+		for(int i = 1; i <= 31; i++) {
+			t.aggiungiAttività(new Attività("Studiare",
+										  LocalDate.of(2021, Month.JANUARY, i),
+										  LocalTime.of(19, 0),
+										  7200L,
+										  t.getProgetti().get(1)));
+		}
+	};
+	
+	/**
+	 * Verifica l'aggiunta di attività e progetti
+	 */
+	@Order(1)
+	@Test
+	public void testAttivita() {
+		t = new TimeTracker();
+		inizializza(t);
+		
+		// ci sono 4 attività
+		assertEquals(4, t.getAttività().size());
+		
+		// ci sono 4 giorni di attività
+		List<List<IAttività>> giorniAttività = t.getGiorniAttività(1);
+		assertEquals(4, t.getNumGiorni());
+		assertEquals(4, giorniAttività.size());
+		List<IAttività> giorno1 = giorniAttività.get(0);
+		List<IAttività> giorno2 = giorniAttività.get(1);
+		
+		// L'ordine dei giorni va dal più recente al meno recente 
+		assertTrue(giorno1.get(0).getData().isAfter(giorno2.get(0).getData()));
+		
+		// La data della prima attività nel primo giorno attività è 7 Marzo 2022, 15:55
+		IAttività a1 = giorno1.get(0);
+		assertEquals(7, a1.getData().getDayOfMonth());
+		assertEquals(Month.MARCH, a1.getData().getMonth());
+		assertEquals(2022, a1.getData().getYear());
+		assertEquals(15, a1.getOraInizio().getHour());
+		assertEquals(55, a1.getOraInizio().getMinute());
+		
+		// La data della prima attività nel secondo giorno attività è 5 Febbraio 2021, 07:02
+		IAttività a2 = giorno2.get(0);
+		assertEquals(5, a2.getData().getDayOfMonth());
+		assertEquals(Month.FEBRUARY, a2.getData().getMonth());
+		assertEquals(2021, a2.getData().getYear());
+		assertEquals(2022, a1.getData().getYear());
+		assertEquals(7, a2.getOraInizio().getHour());
+		assertEquals(2, a2.getOraInizio().getMinute());
+	}
+	
+	/**
+	 * Verifica che vengano calcolati correttamente i tempi annuali e mensili di un progetto
+	 */
+	@Test
+	@Order(2)
+	public void testProgetto() {
+		t = new TimeTracker();
+		inizializza(t);
+		IProgetto p = t.getProgetti().get(1);
+		
+		// ci sono 3 progetti
+		assertEquals(3, t.getProgetti().size());
+		
+		// il primo è quello di default "Altro"
+		IProgetto defP = t.getProgetti().get(0);
+		assertEquals("Altro", defP.getNome());
+		
+		// calcolo i tempi totali del primo progetto per ogni mese dell'anno: 2021
+		Map<Integer, Long> annoAttività = p.getAnnoProgetto(2021);
+		
+		// nel Gennaio del 2021 il tempo totale del progetto ammonta a 4 ore
+		assertEquals((Long)14400L, annoAttività.get(1));
+		
+		// nel Febbraio del 2021 il tempo totale del progetto ammonta a 2 ore
+		assertEquals((Long)7200L, annoAttività.get(2));
+		
+		// calcolo i tempi totali del primo progetto per ogni giorno del mese: Gennaio 2021
+		Map<Integer, Long> meseAttività = p.getMeseProgetto(2021, Month.JANUARY);
+		
+		// il 2 Gennaio 2021 il tempo totale del progetto ammonta a 2 ore
+		assertEquals((Long)7200L, meseAttività.get(2));
+		
+		// a Marzo 2022 il tempo totale ammonta a 1 ora
+		Map<Integer, Long> annoAttività2 = defP.getAnnoProgetto(2022);
+		Map<Integer, Long> meseAttività2 = defP.getMeseProgetto(2022, Month.MARCH);
+		assertEquals((Long)3600L, meseAttività2.get(7));
+		assertEquals((Long)3600L, annoAttività2.get(3));
+	}
+	
+	/**
+	 * Verifica l'eliminazione di progetti
+	 */
+	@Test
+	@Order(3)
+	public void testEliminazioneProgetti() {
+		t = new TimeTracker();
+		inizializza(t);
+		
+		// il time tracker contiene 2 progetti
+		assertEquals(3, t.getProgetti().size());
+		
+		// elimino il terzo progetto
+		t.eliminaProgetto(t.getProgetti().get(2).getId());
+		
+		// il time tracker contiene 2 
+		assertEquals(2, t.getProgetti().size());
+		// il secondo progetto è: "Analisi"
+		assertEquals("Analisi", t.getProgetti().get(1).getNome());
+		
+	}
+//	
+	/**
+	 * Verifica l'eliminazione di attività
+	 */
+	@Test 
+	@Order(4)
+	public void testEliminazioneAttività() {
+		t = new TimeTracker();
+		inizializza(t);
+		
+		// ci sono 4 giorni di attività
+		List<List<IAttività>> giorniAttività = t.getGiorniAttività(1);
+		assertEquals(4, giorniAttività.size());
+		
+		// attività "Studiare ancora" del 5 Febbraio 2021
+		IAttività a = giorniAttività.get(1).get(0);
+		
+		// elimino l'attività
+		t.eliminaAttività(a);
+		
+		// ci sono 3 giorni attività
+		giorniAttività = t.getGiorniAttività(1);
+		assertEquals(3, giorniAttività.size());
+		List<IAttività> giorno = giorniAttività.get(0);
+		List<IAttività> giorno2 = giorniAttività.get(1); 
+		List<IAttività> giorno3 = giorniAttività.get(2);
+		
+		// la prima attività rimanente è "Gambe" del 7 Marzo 2022
+		assertEquals("Gambe", giorno.get(0).getNome());
+		assertEquals(7, giorno.get(0).getData().getDayOfMonth());
+		assertEquals(Month.MARCH, giorno.get(0).getData().getMonth());
+		assertEquals(2022, giorno.get(0).getData().getYear());
+		
+		// la seconda attività rimanente è "Fare esercizi" del 10 Gennaio 2021
+		assertEquals("Fare esercizi", giorno2.get(0).getNome());
+		assertEquals(10, giorno2.get(0).getData().getDayOfMonth());
+		assertEquals(Month.JANUARY, giorno2.get(0).getData().getMonth());
+		assertEquals(2021, giorno2.get(0).getData().getYear());
+		
+		// la terza attività rimanente è "Studiare" del 2 Gennaio 2021
+		assertEquals("Studiare", giorno3.get(0).getNome());
+		assertEquals(2, giorno3.get(0).getData().getDayOfMonth());
+		assertEquals(Month.JANUARY, giorno3.get(0).getData().getMonth());
+		assertEquals(2021, giorno3.get(0).getData().getYear());
+	}
+	
+	/**
+	 * Verifica la paginazione dei giorni di attività
+	 */
+	@Test
+	@Order(5)
+	public void verificaPaginazione() {
+		t = new TimeTracker();
+		aggiungiAttività(t);
+		
+		// ottengo la prima pagina con i primi 10 giorni di attività
+		List<List<IAttività>> giorniAttività = t.getGiorniAttività(1);
+		List<IAttività> giorno1 = giorniAttività.get(0);
+		List<IAttività> giorno2 = giorniAttività.get(1);
+		
+		// il primo giorno della prima pagina è 31 Gennaio
+		assertEquals(31, giorno1.get(0).getData().getDayOfMonth());
+		// il secondo giorno della prima pagina è il 30 Gennaio
+		assertEquals(30, giorno2.get(0).getData().getDayOfMonth());
+		
+		// ottengo la seconda pagina con i successivi 10 giorni di attività
+		giorniAttività = t.getGiorniAttività(2);
+		giorno1 = giorniAttività.get(0);
+		giorno2 = giorniAttività.get(1);
+		
+		// il primo giorno della seconda pagina è il 21 Gennaio
+		assertEquals(21, giorno1.get(0).getData().getDayOfMonth());
+		// il secondo giorno della seconda pagina è il 20 Gennaio
+		assertEquals(20, giorno2.get(0).getData().getDayOfMonth());
+	}
+	
+	/**
+	 * Verifica funzionamento del pomodoro timer
+	 */
+	@Test
+	@Order(6)
+	public void verificaPomodoro1() {
+		t = new TimeTracker();
+		t.scegliTracker(TrackerEnum.POMODOROTIMER);
+		
+		// il timer di default ha
+		// sessione: 30M
+		// pausa breve: 5M
+		// pausa lunga: 10M
+		// cicli prima della pausa lunga: 3
+		IPomodoroTimer tracker = (IPomodoroTimer) t.getTracker();
+		assertEquals(30*60, tracker.getDurataSessione());
+		assertEquals(5*60, tracker.getDurataPausaBreve());
+		assertEquals(10*60, tracker.getDurataPausaLunga());
+		assertEquals(3, tracker.getNCicli());
+		
+		// Avvio il tracker sull'attività "Studiare"
+		t.avviaTracker(new Attività("Studiare",
+									LocalDate.of(2021, Month.JANUARY, 2),
+									LocalTime.of(19, 0),
+									7200L,
+									t.getProgetti().get(0)));
+		
+		// Attendo 1 secondo e qualcosa
+		try {
+			Thread.sleep(1100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} 
+		
+		// Termino il tracker
+		t.terminaTracker();	
+		
+		// E' presente un'attività "Studiare", la cui durata è 1 secondo
+		List<List<IAttività>> giorni = t.getGiorniAttività(1);
+		IAttività a = giorni.get(0).get(0);
+		assertEquals("Studiare", a.getNome());
+		assertEquals(1, a.getDurata());
+	}
+
+	/**
+	 * Verifica funzionamento del pomodoro timer
+	 */
+	@Test
+	@Order(7)
+	public void verificaPomodoro2() {
+		t = new TimeTracker();
+		t.scegliTracker(TrackerEnum.POMODOROTIMER);
+		IPomodoroTimer tracker = (IPomodoroTimer) t.getTracker();
+		
+		// imposto il timer: sessione 2s, pausa breve 1s, pausa lunga 1s, nCicli 1
+		tracker.setDurataSessione(2);
+		tracker.setDurataPausaBreve(1);
+		tracker.setDurataPausaLunga(1);
+		tracker.setNCicli(1);
+		
+		// le impostazioni del timer sono corrette
+		assertEquals(2, tracker.getDurataSessione());
+		assertEquals(1, tracker.getDurataPausaBreve());
+		assertEquals(1, tracker.getDurataPausaLunga());
+		assertEquals(1, tracker.getNCicli());
+		
+		// avvio il tracker sull'attività "Studiare"
+		t.avviaTracker(new Attività("Studiare",
+									LocalDate.of(2021, Month.JANUARY, 5),
+									LocalTime.of(19, 0),
+									7200L,
+									t.getProgetti().get(0)));
+		
+		// Prima sessione, attendo 3 secondi e qualcosa in modo che termini
+		try {
+			Thread.sleep(3100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// è presente un giorno attività, con un'attività "Studiare", durata 2s, data 5 Gennaio 2021
+		List<List<IAttività>> giorni = t.getGiorniAttività(1);
+		assertEquals(1, giorni.get(0).size());
+		assertEquals("Studiare", giorni.get(0).get(0).getNome());
+		assertEquals(2021, giorni.get(0).get(0).getData().getYear());
+		assertEquals(Month.JANUARY, giorni.get(0).get(0).getData().getMonth());
+		assertEquals(5, giorni.get(0).get(0).getData().getDayOfMonth());
+		assertEquals(2, giorni.get(0).get(0).getDurata());
+		
+		// timer in pausa breve (1s) e lunga (1s), attendo 2 secondi e qualcosa
+		try {
+			Thread.sleep(3100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// non sono state aggiunte nuove attività
+		giorni = t.getGiorniAttività(1);
+		assertEquals(1, giorni.get(0).size());
+		
+		// timer in sessione, attendo 2 secondi e qualcosa
+		try {
+			Thread.sleep(4100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// è stata aggiunta un'altra attività, "Studiare", durata 2s, nello stesso giorno 2 Gennaio 2021
+		giorni = t.getGiorniAttività(1);
+		assertEquals(2, giorni.get(0).size());
+		assertEquals("Studiare", giorni.get(0).get(1).getNome());
+		assertEquals(2021, giorni.get(0).get(1).getData().getYear());
+		assertEquals(Month.JANUARY, giorni.get(0).get(1).getData().getMonth());
+		assertEquals(5, giorni.get(0).get(1).getData().getDayOfMonth());
+		assertEquals(2, giorni.get(0).get(1).getDurata());
+		
+		// termino il timer
+		t.terminaTracker();
+		
+		// timer terminato
+		assertFalse(t.getTracker().getAvviato());
+	
+	}
+	
+	/**
+	 * Verifica il funzionamento del cronometro
+	 */
+	@Test 
+	@Order(8)
+	public void verificaCronometro() {
+		t = new TimeTracker();
+		t.scegliTracker(TrackerEnum.CRONOMETRO);
+		ICronometro c = (ICronometro) t.getTracker();
+		
+		// Avvio il tracker sull'attività "Studiare"
+		t.avviaTracker(new Attività("Studiare",
+									LocalDate.of(2021, Month.JANUARY, 2),
+									LocalTime.of(19, 0),
+									7200L,
+									t.getProgetti().get(0)));
+		
+		// sono stati monitorati 0 secondi
+		assertEquals(0, c.getTempoAttuale());
+		
+		// attendo 1 secondo
+		try {
+			Thread.sleep(1100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// sono stati monitorati 1 secondi
+		assertEquals(1, c.getTempoAttuale());
+		
+		// Termino il tracker
+		t.terminaTracker();	
+		
+		// E' presente un'attività "Studiare", la cui durata è 1 secondo
+		List<List<IAttività>> giorni = t.getGiorniAttività(1);
+		IAttività a = giorni.get(0).get(0);
+		assertEquals("Studiare", a.getNome());
+		assertEquals(1, a.getDurata());
+	}
+
+}
